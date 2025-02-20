d95f2be7f68ea0cc5c42323c02fffddbffa638fe
LaxmanRky <laxman.rokaya02@gmail.com>
2025-02-20 16:52:48 -0500

    updated routes

--- routes/songRoutes.js
+++ routes/songRoutes.js
@@ -1,1 +1,10 @@
 
+const express = require('express');
+const router = express.Router();
+const { getAllSongs, getSongById, getSongsByArtist } = require('../Controllers/songController');
+
+router.get('/songs', getAllSongs);
+router.get('/songs/:id', getSongById);
+router.get('/songs/artist/:artist', getSongsByArtist);
+
+module.exports = router;
